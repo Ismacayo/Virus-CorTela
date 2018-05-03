f9dece1401f2a47dabc6870357c2e1244403c1c2
Cayo da Silva Lima <cayosl2013@gmail.com>
2018-05-03 14:26:07 -0300

    Add files via upload

---
+++ Virus Corteela.c
@@ -0,0 +1,123 @@
+#include "meuh.h"
+#define MAX 100
+#define cor1; system("color 1A");
+#define cor2;  system("color 2A"); 
+#define cor3; system("color 3A"); 
+#define cor4;  system("color 4A"); 
+#define cor5;  system("color 5A"); 
+#define cor6;  system("color 6A"); 
+#define cor7;  system("color 7A"); 
+#define cor8; system("color 8A"); 
+#define cor9; system("color 9A"); 
+#define cor10; system("color 1B"); 
+#define cor11; system("color 2B"); 
+#define cor12; system("color 3B"); 
+#define cor13; system("color 4B"); 
+#define cor14; system("color 5B"); 
+#define cor15; system("color 6B"); 
+#define cor16; system("color 7B"); 
+#define cor17; system("color 8B");
+#define cor18; system("color 9B");
+#define cor19; system("color 1C");
+#define cor20; system("color 2C"); 
+#define cor21; system("color 3C"); 
+#define cor22; system("color 4C"); 
+#define cor23; system("color 5C");
+#define cor24; system("color 6C");   
+#define cor25; system("color 7C"); 
+#define cor26; system("color 8C"); 
+#define cor27; system("color 9C");
+#define cor28; system("color 1D");
+#define cor29; system("color 2D"); 
+#define cor30; system("color 3D"); 
+#define cor31; system("color 4D"); 
+#define cor32; system("color 5D"); 
+#define cor33; system("color 6D"); 
+#define cor34; system("color 7D"); 
+#define cor35; system("color 8D"); 
+#define cor36; system("color 9D");   
+#define cor37; system("color 1E"); 
+#define cor38; system("color 2E");
+#define cor39; system("color 3E"); 
+#define cor40; system("color 4E"); 
+#define cor41; system("color 5E"); 
+#define cor42; system("color 6E"); 
+#define cor43; system("color 7E"); 
+#define cor44; system("color 8E"); 
+#define cor45; system("color 9E"); 
+#define cor46; system("color 1F"); 
+#define cor47; system("color 2F");
+#define cor48; system("color 3F");
+#define cor49; system("color 4F");
+#define cor50; system("color 5F");
+#define cor51; system("color 6F");
+#define cor52; system("color 7F");
+#define cor53; system("color 8F");
+#define cor54; system("color 9F");
+
+int x = 0;
+
+main()
+{
+	for(x = 0;x < 56;x ++)
+	{
+
+	if(x == 0) cor1;
+	if(x == 1) cor2;
+	if(x == 2) cor3;
+	if(x == 3) cor4;
+	if(x == 4) cor5;
+	if(x == 5) cor6;
+	if(x == 6) cor7;
+	if(x == 7) cor8;
+	if(x == 8) cor9;
+	if(x == 9) cor10;
+	if(x == 10) cor11;
+	if(x == 11) cor12;
+	if(x == 12) cor13;
+	if(x == 13) cor14;
+	if(x == 14) cor15;
+	if(x == 15) cor16;
+	if(x == 16) cor17;
+	if(x == 17) cor18;
+	if(x == 18) cor19;
+	if(x == 19) cor20;
+	if(x == 20) cor21;
+	if(x == 21) cor22;
+	if(x == 22) cor23;
+	if(x == 23) cor24;
+	if(x == 24) cor25;
+	if(x == 25) cor26;
+	if(x == 26) cor27;
+	if(x == 27) cor28;
+	if(x == 28) cor29;
+	if(x == 29) cor30;
+	if(x == 30) cor31;
+	if(x == 31) cor32;
+	if(x == 32) cor33;
+	if(x == 33) cor34;
+	if(x == 34) cor35;
+	if(x == 35) cor36;
+	if(x == 36) cor37;
+	if(x == 37) cor38;
+	if(x == 38) cor39;
+	if(x == 39) cor40;
+	if(x == 40) cor41;
+	if(x == 41) cor42;
+	if(x == 42) cor43;
+	if(x == 43) cor44;
+	if(x == 44) cor45;
+	if(x == 45) cor46;
+	if(x == 46) cor47;
+	if(x == 47) cor48;
+	if(x == 48) cor49;
+	if(x == 49) cor50;
+	if(x == 50) cor51;
+	if(x == 51) cor52;
+	if(x == 52) cor53;
+	if(x == 53) cor54;
+	printf("010101");
+	if(x == 55) x = 0;
+	}
+	
+}
